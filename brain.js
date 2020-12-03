671b99fbfd736d059de0076df70e61f006347307
Jeff Priso <jeffpriso@macbook-air.attlocal.net>
2020-12-03 18:16:49 -0500

    first commit

---
+++ brain.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $("#myblog").click(function(){
+        var url="https://brooklynnmichelle.com/"
+      window.open(url,"blank")
+    });
+    $("#submit").click(function(){
+        alert("Message submitted successfully.")
+    })
+  });
